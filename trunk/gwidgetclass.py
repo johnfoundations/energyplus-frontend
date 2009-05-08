d4bd5c574d4cd41051aa7a7a45bb11b6ff8deaa4
derekkite <derekkite@d95564f8-3603-11de-82fb-7f665ef830f7>
2009-05-08 03:59:03 +0000

    Set up calculations within vertice widget. Sets values of width and height and vertice elements.

--- trunk/gwidgetclass.py
+++ trunk/gwidgetclass.py
@@ -4,6 +4,7 @@
 import idfglobals
 from PyQt4 import QtGui, QtCore
 import sys
+import math
 
 
 
@@ -172,6 +173,7 @@ class GVerticeWidget(QtGui.QWidget):
     hl.addWidget(self.brxyz)
     vlayout.addLayout(hl)
     self.connectSignal()
+    self.fieldmatrix = []
     
   def connectSignal(self):
     self.connect(self.verticecount, QtCore.SIGNAL('valueChanged (int)'),self.changedCount)
@@ -189,6 +191,42 @@ class GVerticeWidget(QtGui.QWidget):
     stri = '%g,%g,%g'%(arr[0],arr[1],arr[2])
     return stri
 
+  def getFieldMatrix(self):
+    vmatrix = []
+    vmatrix.append(self.buildVerticeArray(self.blxyz.text()))
+    vmatrix.append(self.buildVerticeArray(self.brxyz.text()))
+    vmatrix.append(self.buildVerticeArray(self.trxyz.text()))
+    vmatrix.append(self.buildVerticeArray(self.tlxyz.text()))
+    fill = [0.0,0.0,0.0]
+    for c,l in enumerate(vmatrix):
+      if len(l) == 0:
+        vmatrix[c] = fill
+    return vmatrix
+
+  def setFieldMatrix(self,matrix):
+    self.blxyz.setText(self.buildVerticeString(matrix[0]))
+    self.brxyz.setText(self.buildVerticeString(matrix[1]))
+    self.trxyz.setText(self.buildVerticeString(matrix[2]))
+    self.tlxyz.setText(self.buildVerticeString(matrix[3]))
+    self.fieldmatrix = matrix
+
+
+  def setValue(self,m):
+    wv = self.transform(m[0],m[1])
+    self.width.setText(abs(dist(wv)))
+    hv = self.transform(m[0],m[3])
+    self.height.setText(abs(dist(hv)))
+    self.setFieldMatrix(m)
+    self.width
+    
+  def dist(self,v):
+    return math.sqrt(math.pow(v[0],2) + math.pow(v[1],2) + math.pow(v[2],2))
+    
+
+  def getValue(self):
+    return self.fieldmatrix
+
+
   def buildVerticeArray(self,s):
     l = s.split(',')
     f = []
@@ -204,42 +242,121 @@ class GVerticeWidget(QtGui.QWidget):
       f.append(float(l[2]))
     except:
       return []
-    print f
     return f
 
+  def transform(self,v1,v2):
+    res = [0,0,0]
+    res[0] = v2[0] - v1[0]
+    res[1] = v2[1] - v1[1]
+    res[2] = v2[2] - v1[2]
+    return res
+
+  def add(self,v1,v2)  :
+    res = [0,0,0]
+    res[0] = v2[0] + v1[0]
+    res[1] = v2[1] + v1[1]
+    res[2] = v2[2] + v1[2]
+    return res
+
+  def mult(self,v1,m):
+    res = [0,0,0]
+    res[0] = v1[0] *m
+    res[1] = v1[1] *m
+    res[2] = v1[2] *m
+    return res
+    
   def changedWidth(self):
     n = float(self.width.text())
     bla = self.buildVerticeArray(self.blxyz.text())
     if not len(bla) == 3:
-      bla = self.buildVerticeArray('0.0,0.0,0.0')
-      self.blxyz.setText(self.buildVerticeString(bla))
-    bra = self.buildVerticeArray(self.brxyz.text())
-    if not len(bra) == 3:
-      bra = self.buildVerticeArray('0.0,0.0,0.0')
-    bra[0] = bla[0] + n
-    self.brxyz.setText(self.buildVerticeString(bra))
-    tra = self.buildVerticeArray(self.trxyz.text())
-    if not len(tra) == 3:
-      tra = self.buildVerticeArray('0.0,0.0,0.0')
-    tra[0] = bra[0]
-    self.trxyz.setText(self.buildVerticeString(tra))
-      
-    
+      return
+    else:
+      matrix = self.getFieldMatrix()
+      normbr = self.transform(matrix[0],matrix[1])
+      dist = self.dist(normbr)
+      if dist == 0:
+        delta = n
+      else:
+        delta = n/dist
+      br =  self.mult(normbr,delta)
+      matrix[1] = self.add(br,matrix[0])
+      normbr = self.transform(matrix[3],matrix[2])
+      print normbr
+      dist = self.dist(normbr)
+      print dist
+      if dist == 0:
+        delta = n
+      else:
+        delta = n/dist
+      print delta
+      br =  self.mult(normbr,delta)
+      print br
+      matrix[2] = self.add(br,matrix[3])
+      self.setFieldMatrix(matrix)
+
   def changedHeight(self):
-    n = float(i)
-    print n
+    n = float(self.height.text())
+    bla = self.buildVerticeArray(self.blxyz.text())
+    if not len(bla) == 3:
+      return
+    else:
+      matrix = self.getFieldMatrix()
+      normbr = self.transform(matrix[0],matrix[3])
+      dist = math.sqrt(math.pow(normbr[0],2) + math.pow(normbr[1],2) + math.pow(normbr[2],2))
+      if dist == 0:
+        delta = n
+      else:
+        delta = n/dist
+      br =  self.mult(normbr,delta)
+      matrix[3] = self.add(br,matrix[0])
+      normbr = self.transform(matrix[1],matrix[2])
+      dist = math.sqrt(math.pow(normbr[0],2) + math.pow(normbr[1],2) + math.pow(normbr[2],2))
+      if dist == 0:
+        delta = n
+      else:
+        delta = n/dist
+      br =  self.mult(normbr,delta)
+      matrix[2] = self.add(br,matrix[1])
+      self.setFieldMatrix(matrix)
+        
     
   def changedtl(self):
-    print i
+    pass
     
   def changedtr(self):
-    print i
+    pass
     
   def changedbr(self):
-    print i
+    matrix = self.getFieldMatrix()
+    if len(self.fieldmatrix) == 0:
+      matrix[0] = [0,0,0]
+      matrix[2] = [1,0,1]
+      matrix[3] = [0,0,1]
+      self.setFieldMatrix(matrix)
+      self.changedWidth()
+      self.changedHeight()
+      return
+    diff = self.transform(self.fieldmatrix[1],matrix[1])
+    matrix[2] = self.add(matrix[2],diff)
+    self.setFieldMatrix(matrix)
+      
     
   def changedbl(self):
-    print i
+    matrix = self.getFieldMatrix()
+    if len(self.fieldmatrix) == 0:
+      matrix[1] = [1,0,0]
+      matrix[2] = [1,0,1]
+      matrix[3] = [0,0,1]
+      self.setFieldMatrix(matrix)
+      self.changedWidth()
+      self.changedHeight()
+      return
+    diff = self.transform(self.fieldmatrix[0],matrix[0])
+    matrix[1] = self.add(matrix[1],diff)
+    matrix[2] = self.add(matrix[2],diff)
+    matrix[3] = self.add(matrix[3],diff)
+    self.setFieldMatrix(matrix)
+    
 
 
 
