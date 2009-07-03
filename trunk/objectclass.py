d1ea6c31a521628abd2be08a531e459305e50122
a88gerber <a88gerber@d95564f8-3603-11de-82fb-7f665ef830f7>
2009-07-03 15:38:04 +0000

    indentation for some files changed from 2 to 4 blanks models can now be written (objectclass.py, idfread.py)

--- trunk/objectclass.py
+++ trunk/objectclass.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""***************************************************************************
-*   Copyright (C) 2009 by Derek Kite   *
-*   dkite@shaw.ca   *
+"""************************************************************************
+*   Copyright (C) 2009 by Derek Kite                                      *
+*   dkite@shaw.ca                                                         *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
@@ -32,196 +32,233 @@ from fieldclasses import *
 #each one is created as a subclass of ObjectAbstract,
 #and it's init will create the fields and append them to the field list
 class ObjectAbstract :
-  def __init__(self):
-    self.classn = ''
-    self.name = ''
-    self.fieldlist = []
-    self.required = False
-    self.referencelist = []
-    self.rawdatalist = []
-    self.group = ''
-    self.extensible = False
-    self.editwidget = None
-    self.scrollarea = None
-    self.memo = []
-    self.CreateFields()
+    def __init__(self):
+        self.classn = ''
+        self.name = ''
+        self.fieldlist = []
+        self.required = False
+        self.referencelist = []
+        self.rawdatalist = []
+        self.group = ''
+        self.extensible = False
+        self.editwidget = None
+        self.scrollarea = None
+        self.memo = []
+        self.CreateFields()
+        
+    def setName(self,name):
+        self.name = name
+
+    def getName(self):
+        if self.name == '':
+            return self.getClassnameIDD()
+        else:
+            return self.name
+
+    def setClassnameIDD(self,name):
+        self.classn = name
+
+    def getClassnameIDD(self):
+        return self.classn
     
-  def setName(self,name):
-    self.name = name
-
-  def getName(self):
-    if self.name == '':
-      return self.getClassnameIDD()
-    else:
-      return self.name
-
-  def setClassnameIDD(self,name):
-    self.classn = name
-
-  def getClassnameIDD(self):
-    return self.classn
-  
-  def InsertField(self,field):
-    self.fieldlist.append(field)
-  
-  def setData(self,data):
-    self.rawdatalist = data
-    self.ParseRawData()
-
-  def getData(self) :
-    print self.getClassnameIDD() + ' getData not implemented'
+    def InsertField(self,field):
+        self.fieldlist.append(field)
     
-  def CreateEditWidget(self) :
-    self.editwidget = QtGui.QWidget()
-    vlayout = QtGui.QVBoxLayout(self.editwidget)
-#    hlayout = QtGui.QVBoxLayout()
-    label = QtGui.QLabel(self.getClassnameIDD())
-    ms = ''
-    for m in self.memo:
-      ms = ms + m + '\n'
-    ms = ms.strip()
-    label.setToolTip(ms)
-    vlayout.addWidget(label)
-    count = 0
-    for f in self.fieldlist:
-      count = count + 1
-      vlayout.addWidget(f.CreateEditor())
-      f.setEditorValue()
-      if count > 2:
-	      count = 0
-#      vlayout.addStretch()
-#      hlayout.addLayout(vlayout)
-#      vlayout = QtGui.QVBoxLayout()
-#      vlayout.addStretch()
-#      hlayout.addLayout(vlayout)
-    vlayout.addStretch()
-    self.scrollarea = QtGui.QScrollArea()
-    self.scrollarea.setWidget(self.editwidget)
-    self.scrollarea.setWidgetResizable(True)
-    return self.scrollarea
+    def setData(self,data):
+        self.rawdatalist = data
+        self.ParseRawData()
 
-  def closeWidget(self):
-    if self.scrollarea :
-      self.scrollarea.close()
+    def getData(self) :
+        print self.getClassnameIDD() + ' getData not implemented'
+        
+    def CreateEditWidget(self) :
+        self.editwidget = QtGui.QWidget()
+        vlayout = QtGui.QVBoxLayout(self.editwidget)
+#        hlayout = QtGui.QVBoxLayout()
+        label = QtGui.QLabel(self.getClassnameIDD())
+        ms = ''
+        for m in self.memo:
+            ms = ms + m + '\n'
+        ms = ms.strip()
+        label.setToolTip(ms)
+        vlayout.addWidget(label)
+        count = 0
+        for f in self.fieldlist:
+            count = count + 1
+            vlayout.addWidget(f.CreateEditor())
+            f.setEditorValue()
+            if count > 2:
+                count = 0
+#            vlayout.addStretch()
+#            hlayout.addLayout(vlayout)
+#            vlayout = QtGui.QVBoxLayout()
+#            vlayout.addStretch()
+#            hlayout.addLayout(vlayout)
+        vlayout.addStretch()
+        self.scrollarea = QtGui.QScrollArea()
+        self.scrollarea.setWidget(self.editwidget)
+        self.scrollarea.setWidgetResizable(True)
+        return self.scrollarea
 
-  def CreateFields(self) :
-    print 'to be subclassed'
+    def closeWidget(self):
+        if self.scrollarea :
+            self.scrollarea.close()
 
-  def PrintIDF(self,fh) :
-    print(self.getClassnameIDD() + ',\n')
-    for c,fn in enumerate(self.fieldlist):
-      if c < len(self.fieldlist)-1:
-        sep = ','
-      else:
-        sep= ';'
-      fn.printIDF(sep)
-      
+    def CreateFields(self) :
+        print 'to be subclassed'
+
+    def PrintIDF(self,fh) :
+        print(self.getClassnameIDD() + ',\n')
+        for c,fn in enumerate(self.fieldlist):
+            if c < len(self.fieldlist)-1:
+                sep = ','
+            else:
+                sep= ';'
+            fn.printIDF(sep)
+
+    def __str__(self):            
+        lines = []
+        lines.append(self.getClassnameIDD() + ',')
+        tmplines = []
+        for c, field in enumerate(self.fieldlist):
+            if c > self.minfields and field.value == '':
+                tmplines.append('    , !- %s' % field.fieldname)
+            else:
+                lines.extend(tmplines)
+                tmplines = []
+                lines.append('    %s, ! %s' % (field, field.fieldname))
+#                if field.restoflist:
+#                    print 'restoflist', field.fieldname, len(field.restoflist)
+        if self.extensible and field.restoflist:
+            newlines = []
+            for i in range(0, len(field.restoflist), self.extensible):
+                line = ''
+                for idx in range(self.extensible):
+                    line += str(field.restoflist[i+idx]) + ','
+                newlines.append(line)
+            try:
+                newlines[-1] = newlines[-1][:-1] + ';'
+                lines.extend(newlines)
+            except IndexError:
+                pass
+        else:
+            lines[-1] = lines[-1].replace(',', ';',1)
+        return '\n'.join(lines) + '\n'
+            
+#        for c, field in enumerate(self.fieldlist):
+#            if c < len(self.fieldlist)-1 :
+#                sep = ','
+#            else:
+#                sep= ';'
+#            s += '    %s%s ! %s\n' % (str(field), sep, field.fieldname)
+        return s
 
 
-  def setRequired(self,req) :
-    self.required = req
-  
-  def getRequired(self):
-    return self.required
+    def setRequired(self,req) :
+        self.required = req
+    
+    def getRequired(self):
+        return self.required
 
-  def setMinfields(self,num):
-    self.minfields = num
+    def setMinfields(self,num):
+        self.minfields = num
 
-  def getMinfields(self):
-    return self.minfields
+    def getMinfields(self):
+        return self.minfields
 
-  def insertMemo(self,memo):
-    self.memo = memo
+    def insertMemo(self,memo):
+        self.memo = memo
 
-  def getMemo(self) :
-    return self.memo
+    def getMemo(self) :
+        return self.memo
 
-  def addReference(self,ref):
-    self.referencelist.append(ref)
+    def addReference(self,ref):
+        self.referencelist.append(ref)
 
-  def getReference(self) :
-    return self.referencelist
+    def getReference(self) :
+        return self.referencelist
 
-  def setGroup(self,g) :
-    self.group = g
+    def setGroup(self,g) :
+        self.group = g
 
-  def getGroup(self):
-    return self.group
+    def getGroup(self):
+        return self.group
 
-  def setExtensible(self,num) :
-    self.extensible = num
+    def setExtensible(self,num) :
+        self.extensible = num
 
-  def getDepends(self):
-    dependslist = []
-    for f in self.fieldlist :
-      if f.getFieldDepends():
-        dependslist.append(f.getFieldDepends())
-    return dependslist
+    def getDepends(self):
+        dependslist = []
+        for f in self.fieldlist :
+            if f.getFieldDepends():
+                dependslist.append(f.getFieldDepends())
+        return dependslist
 
-  def getObjectDepend(self):
-    for f in self.fieldlist:
-      if f.getFieldDepends() :
-        if f.getFieldName() == 'Zone Name':
-          return f.getValue()
-      
+    def getObjectDepend(self):
+        for f in self.fieldlist:
+            if f.getFieldDepends() :
+                if f.getFieldName() == 'Zone Name':
+                    return f.getValue()
+            
 
-  def ParseRawData(self) :
-    remainder = []
-    remainder = self.rawdatalist[:]
-#    print remainder
-#    pdb.set_trace()
-    for i in range(len(self.rawdatalist)):
-      if i == 0:
-        del remainder[0]
-        continue
+    def ParseRawData(self) :
+        remainder = []
+        remainder = self.rawdatalist[:]
+#        print remainder
+#        pdb.set_trace()
+        for i in range(len(self.rawdatalist)):
+            if i == 0:
+                del remainder[0]
+                continue
 
-      if i <= len(self.fieldlist):
-        f = self.fieldlist[i-1]
-      else:
-        if not self.extensible:
-          pdb.set_trace()
-          print 'ran out of fieldlists ' + self.getClassnameIDD()
-        return
-      d = self.rawdatalist[i]
-      del remainder[0]
-      if not f.setValue(d,remainder):
-        print 'parserawdata setvalue false ' + d + ' ' + self.getClassnameIDD()
-        return
+            if i <= len(self.fieldlist):
+                f = self.fieldlist[i-1]
+            else:
+                if not self.extensible:
+                    pdb.set_trace()
+                    print 'ran out of fieldlists ' + self.getClassnameIDD()
+                return
+                    
+            d = self.rawdatalist[i]
+            del remainder[0]
+            if not f.setValue(d,remainder):
+                print 'parserawdata setvalue false ' + d + ' ' + self.getClassnameIDD()
+                return
 
 
 class ObjectVertice(ObjectAbstract):
-  pass
-    
-      
+    pass
+        
+            
 
 class ObjectViewFactor(ObjectAbstract):
-  pass
-    
-     
+    pass
+        
+         
 
 class ObjectFluidProperty(ObjectAbstract) :
-  pass
-    
-      
+    pass
+        
+            
 
 class ObjectSingleLine(ObjectAbstract) :
-  pass
-    
-      
+    pass
+        
+            
 
 class ObjectCompactSchedule(ObjectAbstract) :
-  def __init__(self):
-    ObjectAbstract.__init__(self)
-    f = self.fieldlist.pop()
-    del f
-    self.InsertField(FieldCompactSchedule(self,"Schedule","",""))
-    
+    def __init__(self):
+        ObjectAbstract.__init__(self)
+        f = self.fieldlist.pop()
+        del f
+        self.InsertField(FieldCompactSchedule(self,"Schedule","",""))
+        
 
 class ObjectSpectral(ObjectAbstract) :
-  pass
-  
+    pass
     
-      
-
         
-  
+            
+
+                
+    
