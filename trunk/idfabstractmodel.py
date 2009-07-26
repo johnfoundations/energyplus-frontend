90f774876f44fb3f0945898859b0eb83eb9a5fb5
derekkite <derekkite@d95564f8-3603-11de-82fb-7f665ef830f7>
2009-07-26 04:42:05 +0000

    Saving work. Implementation of a model. idfread is used to generate an array of idf class instances, idfdata provides access methods to read the array, and insert into a tree, which the idfabstractmodel uses to traverse and display the data from the classes. Not completed yet.

--- trunk/idfabstractmodel.py
+++ trunk/idfabstractmodel.py
@@ -20,69 +20,135 @@
 ***************************************************************************"""
 
 from PyQt4 import QtCore
+import idfglobals
 
 
 class idfAbstractModel(QtCore.QAbstractItemModel):
-    def __init__(source,parent = 0)
-        QAbstractItemModel (self, parent)
+    def __init__(self,source,parent = None):
+        QtCore.QAbstractItemModel.__init__(self, parent)
         self.idfsource = source
 
     def setIdfSource(self,source):
       self.idfsource = source
 
 
-    def columnCount (self, modelindexparent=0):
-        if modleindexparent == 0:
-            return 2
-        else:
-            #if parent is first column, no children
-            if modelindexparent.column() = 1:
-                instance = self.data(modelindexparent,IdfClassRole)
-                return instance.fieldcount()
-            else:
-                return 2
+    def columnCount (self, parent):
+#       print "columnCount "+str(parent.row())+" "+str(parent.column())
+#        if not parent.isValid():
+        return 2
+#        if parent.isValid():
+#            return parent.internalPointer().childCount()
 
         
-    def flags(self,modelindex):
+    def flags(self,index):
+#        print "flags"
+        if not index.isValid():
+            return QtCore.Qt.ItemIsEnabled
+        else:
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        
 
     def data(self,modelindex,role):
-        if modelindex.row() > self.idfsource.size():
-            return QtCore.QVariant()
-
+        print "data" + str(role)+" "+str(modelindex.row())+" "+str(modelindex.column())
+        
         #retrieve class from idfsource
-        idfclass = self.idfsource.recordAt(modelindex.row()
-        if idfclass == None:
-            return QtCore.QVariant()
+        idf = modelindex.internalPointer()
+        idfclass = self.idfsource.dataAt(idf.row,idf.column)
 
-        if role == IdfClassRole:
-            return idfclass
+        if role == QtCore.Qt.ToolTipRole:
+            return QtCore.QVariant(idfclass.getMemo())
 
-        if role == QtCore.ToolTipRole:
-            return QtCore.QString(idfclass.getMemo())
+        if role == QtCore.Qt.EditRole or role == QtCore.Qt.DisplayRole:
+            if not idf.parent() == 0:
+                if modelindex.column() == 0:
+                    #get field name
+                    return QtCore.QVariant(idfclass.fieldlist[idf.column].fieldname)
 
-        if role == QtCore.EditRole or role == QtCore.DisplayRole:
-            if modelindex.column() > idfclass.fieldCount():
-                return QtCore.QVariant()
+                elif modelindex.column() == 1:
+                    return QtCore.QVariant(idfclass.fieldlist[idf.column].value)
 
-            return QtCore.QString(idfclass.getFieldData(modelindex.column())
+            else:
+                if modelindex.column() == 0:
+                    return QtCore.QVariant(idfclass.getClassnameIDD())
+
+                elif modelindex.column() == 1:
+                    return QtCore.QVariant(idfclass.getName())
+
+        return QtCore.QVariant()
+
+    def headerData(self,section,orientation,role = QtCore.Qt.DisplayRole):
+#        print "headerData"
+#        print section
+#        print orientation
+#        print role
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+             if section == 0:
+                 return QtCore.QVariant("IDDClass")
+             else:
+                 return QtCore.QVariant("Name")
+        return QtCore.QVariant()
+        
+
+    def rowCount (self,parent ):
+        print "rowCount"+ str(parent.row())+" "+str(parent.column())
+        if parent.isValid():
+            return parent.internalPointer().childCount()
+        else:
+            #not valid model.??
+            return self.idfsource.size()
             
 
-    def headerData
+    def parent(self,index):
+        dp = index.internalPointer().parent()
+        if dp == 0:
+            return QtCore.QModelIndex()
+        else:
+            return self.createIndex(dp.row,dp.column,dp)
+        
+ 
 
-    def rowCount
+#    def setData
 
-    def setData
+#    def setHeaderData
 
-    def setHeaderData
+#    def insertRows
 
-    def insertRows
+#    def removeRows
 
-    def removeRows
+#    def insertColumns
 
-    def insertColumns
+#    def removeColumns
 
-    def removeColumns
+    def index(self, row, column, parent):
+        print "index" + str(row) + " " + str(column) +" " + str(parent.row()) +" " + str(parent.column())
+        if parent.isValid():
+            dp = parent.internalPointer().child(row)
+                
+        else:
+            dp = self.idfsource.recordAt(row,column)
 
-    def createIndex
+        return self.createIndex(row, column, dp)
+ 
+        
 
-    
+if __name__ == "__main__":
+    import sys
+    import idfdata
+    from PyQt4 import QtGui
+
+    app = QtGui.QApplication(sys.argv)
+    try:
+        fname = sys.argv[1]
+    except:
+        fname = 'Singlezonetemplate.idf'
+
+    f = idfdata.idfData()
+    f.openIdf(fname)
+
+    model = idfAbstractModel(f)
+
+    view = QtGui.QTreeView()
+    view.setModel(model)
+    view.setWindowTitle("Simple Tree Model")
+    view.show()
+    sys.exit(app.exec_())
