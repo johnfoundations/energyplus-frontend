396cb30d8021c89ee74bc5264fb8125d13dde87c
derekkite <derekkite@d95564f8-3603-11de-82fb-7f665ef830f7>
2009-08-25 02:05:53 +0000

    Creating editor for Compact:Schedule class.
    Using the table and view for all classes, the setData routine will build fields for all the Until:, For: etc. fields. The idea is to allow addition of new data fields according to the format of the class.
    
    Saving work. Fundamentals are there, but it doesn't work yet.

--- compactschedulewidget.py
+++ compactschedulewidget.py
@@ -20,9 +20,11 @@
 ***************************************************************************"""
 
 
-from gwidgetclass import *
+from  PyQt4 import QtGui,QtCore
 import re
 import pdb
+import sys
+from fieldclasses import *
 
     #rules:
       #Through: 12/31 means only one through field
@@ -39,193 +41,191 @@ import pdb
         #numeric value, then done
 
 
-class GCompactScheduleWidget(QtGui.QWidget):
-  def __init__(self,label,parent=None) :
-    QtGui.QWidget.__init__(self,parent)
-    self.throughwidgetlist = []
-    self.throughwidgetlist.append(GThroughWidget())
-    self.vlayout = QtGui.QVBoxLayout(self)
-    self.vlayout.addWidget(self.throughwidgetlist[0])
+
+class compactScheduleHandler():
+    def __init__(self,parent,fieldlist) :
+        self.throughlist = []
+        self.flist = fieldlist
+        self.parent = parent
+        self.throughlist.append(throughSection(parent,self.flist))
 
     
-  def setValue(self,value):
-    #print value
-    throughdata = []
-    for v in value:
-      v = v.strip()
-      res = re.match(r"^Through:(.*)",v)
-      if res:
-        t = res.group(1)
-        if len(throughdata) == 0:
-          throughdata.append(t)
-          continue
-        else:
-          self.throughwidgetlist[-1].setValue(throughdata)
-          throughdata = []
-          throughdata.append(t)
-          self.throughwidgetlist.append(GThroughWidget())
-          self.vlayout.addWidget(self.throughwidgetlist[-1])
-          continue
-      throughdata.append(v)
-    self.throughwidgetlist[-1].setValue(throughdata)
-    self.throughwidgetlist.append(GThroughWidget())
-    self.vlayout.addWidget(self.throughwidgetlist[-1])
+    def setValue(self,value):
+        #print value
+        throughdata = []
+        for v in value:
+            v = v.strip()
+            res = re.match(r"^Through:(.*)",v)
+            if res:
+                t = res.group(1)
+                if len(throughdata) == 0:
+                    throughdata.append(v)
+                    continue
+
+                else:
+                    self.throughlist[-1].setValue(throughdata)
+                    throughdata = []
+                    throughdata.append(t)
+                    self.throughlist.append(throughSection(self.parent,self.flist))
+                    continue
+
+            throughdata.append(v)
+
+        self.throughlist[-1].setValue(throughdata)
+        self.throughlist.append(throughSection(self.parent,self.flist))
 
 
 
-class GThroughWidget(QtGui.QWidget):
-  def __init__(self,parent=None):
-    QtGui.QWidget.__init__(self,parent)
-    self.vlayout = QtGui.QVBoxLayout(self)
-    self.throughedit = GEditWidget('Through')
-    self.vlayout.addWidget(self.throughedit)
-    self.forwidgetlist = []
-    self.forwidgetlist.append(GForWidget())
-    self.vlayout.addWidget(self.forwidgetlist[0])
+class throughSection():
+    def __init__(self,parent,fieldlist):
+        self.flist = fieldlist
+        self.forlist = []
+        self.parent = parent
+        self.forlist.append(ForSection(parent,self.flist))
+        self.throughfield = FieldThrough('','Through:','','')
 
-  def setValue(self,value):
-    #value is an array of for, until and data lines
-    print value
-    widgetindex = 0
-    ds = []
-    self.throughedit.setValue(value[0])
-    for i in value:
-      if widgetindex == 0:
-        widgetindex = 1
-        continue
+
+    def setValue(self,value):
+        #value is an array of for, until and data lines
+        print value
+        index = 0
+        ds = []
+        self.throughfield.setValue(value[0])
+        self.flist.append(self.throughfield)
+        for i in value:
+            if index == 0:
+                index = 1
+                continue
+
+            #pdb.set_trace()
+            i = i.strip()
+            res = re.match(r"^For:(.*)",i)
+            if res:
+                t = res.group(1)
+                #pdb.set_trace()
+                ta = t.split()
+                if len(ds) == 0:
+                    ds.append('For:')
+                    ds = ds + ta
+                    #print ds
+                    continue
+
+                else:
+                    self.forlist[-1].setValue(ds)
+                    self.forlist.append(ForSection(self.parent,self.flist))
+                    ds = []
+                    ds.append('For:')
+                    ds = ds + ta
+                    #print ds
+
+            ds.append(i)
+
+        self.forlist[-1].setValue(ds)
+
+
+
+
+class ForSection():
+    def __init__(self,parent,fieldlist) :
+        self.parent = parent
+        self.flist = fieldlist
+        self.fordatalist = [' ','AllDays','Weekdays','Weekends','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Holiday',\
+                            'SummerDesignDay','WinterDesignDay','AllOtherDays']
+        self.lfordatalist =[]
+        for fd in self.fordatalist:
+            self.lfordatalist.append(fd.lower())
+
+        self.fdlist = self.fordatalist[:]
+        self.forfieldlist = []
+        self.forfieldlist.append(FieldChoice('','For:','','',self.fordatalist))
+        self.populateForCombo()
+        l = QtGui.QLabel('Interpolate')
+        self.interpolatefield = FieldYesNo('','Interpolate:','','',['No','Yes'])
+        self.flist.append(self.interpolatefield)
+        self.lastuntilfield = len(self.flist)
+        self.lock = False
+    
+    
+
+    def populateForCombo(self):
+        for c,i in enumerate(self.forfieldlist):
+            self.insertFor(i)
       
-      #pdb.set_trace()
-      i = i.strip()
-      res = re.match(r"^For:(.*)",i)
-      if res:
-        t = res.group(1)
-        #pdb.set_trace()
-        ta = t.split()
-        if len(ds) == 0:
-          ds.append('For:')
-          ds = ds + ta
-          #print ds
-          continue
-        else:
-          self.forwidgetlist[-1].setValue(ds)
-          self.forwidgetlist.append(GForWidget())
-          self.vlayout.addWidget(self.forwidgetlist[-1])
-          ds = []
-          ds.append('For:')
-          ds = ds + ta
-          #print ds
-      ds.append(i)
-    self.forwidgetlist[-1].setValue(ds)
+    def insertFor(self,field) :
+        self.flist.append(field)
 
+    def insertUntil(self,vals):
+        #print vals
+        if len(vals) < 2:
+            vals = ['','']
 
+        f = FieldTime(self,'Until:','','')
+        t = vals[0].split(':')
+        if len(t) == 2:
+            f.setValue(t[1])
+        self.flist.insert(self.lastuntilfield,f)
+        self.lastuntilfield = self.lastuntilfield + 1
 
+        self.flist.insert(self.lastuntilfield,FieldText(self,'Data:','',''))
+        self.lastuntilfield = self.lastuntilfield + 1
 
-class GForWidget(QtGui.QWidget):
-  def __init__(self,parent=None) :
-    QtGui.QWidget.__init__(self,parent)
-    self.vlayout = QtGui.QVBoxLayout(self)
-    self.fordatalist = [' ','AllDays','Weekdays','Weekends','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Holiday',\
-                        'SummerDesignDay','WinterDesignDay','AllOtherDays']
-    self.lfordatalist =[]
-    for fd in self.fordatalist:
-      self.lfordatalist.append(fd.lower())
-    self.fdlist = self.fordatalist[:]
-    self.forcombolist = []
-    self.forcombovlayout = QtGui.QVBoxLayout()
-    self.forcombolist.append(QtGui.QComboBox())
-    self.vlayout.addLayout(self.forcombovlayout)
-    self.interpolatewidget = GComboBox('Interpolate')
-    self.interpolatewidget.addItems(['No','Yes'])
-    self.vlayout.addWidget(self.interpolatewidget)
-    self.signalmapper = QtCore.QSignalMapper(self)
-    self.connect(self.signalmapper,QtCore.SIGNAL('mapped(int)'),self.forChanged)
-    self.populateForCombo()
-    self.lastuntilwidget = None
-#    self.insertUntil(['',''])
-    self.lock = False
-    
-    
-
-  def populateForCombo(self):
-    for c,i in enumerate(self.forcombolist):
-      self.insertFor(i,c)
-      
-  def insertFor(self,widget,c) :
-    widget.addItems(self.fordatalist)
-    self.connect(widget, QtCore.SIGNAL('currentIndexChanged ( const QString)'),self.signalmapper, QtCore.SLOT('map()'))
-    self.signalmapper.setMapping(widget,c)
-    hlayout = QtGui.QHBoxLayout()
-    hlayout.addWidget(QtGui.QLabel('For:'))
-    hlayout.addWidget(widget)
-    self.forcombovlayout.addLayout(hlayout)
-
-  def insertUntil(self,vals):
-    #print vals
-    if len(vals) < 2:
-      vals = ['','']
-    tw = GTimeWidget('Until:')
-    tw.setValue(vals[0])
-    self.vlayout.addWidget(tw)
-    if not self.lastuntilwidget == None:
-      #print self.lastuntilwidget
-      self.disconnect(self.lastuntilwidget,QtCore.SIGNAL('editingFinished ()'),self.untilEdit)
-    self.lastuntilwidget = GEditWidget('Data:')
-    self.lastuntilwidget.setValue(vals[1])
-    self.connect(self.lastuntilwidget,QtCore.SIGNAL('editingFinished ()'),self.untilEdit)
-    self.vlayout.addWidget(self.lastuntilwidget)
-    
-
-  def untilEdit(self):
-    s = self.lastuntilwidget.text()
-    if not s == '':
-      self.insertUntil(['',''])
+    def untilEdit(self):
+        s = self.lastuntilwidget.text()
+        if not s == '':
+            self.insertUntil(['',''])
   
 
-  def forChanged(self,i):
-    if self.lock:
-      return
-    s = self.forcombolist[i].currentText()
-    #print s
-    if i == len(self.forcombolist)-1:
-      #last
-      w = QtGui.QComboBox()
-      self.forcombolist.append(w)
-      self.insertFor(w,i+1)
+    def forChanged(self,i):
+        if self.lock:
+            return
+
+        s = self.forcombolist[i].currentText()
+        #print s
+        if i == len(self.forcombolist)-1:
+            #last
+            w = QtGui.QComboBox()
+            self.forcombolist.append(w)
+            self.insertFor(w,i+1)
         
-  def setValue(self,v):
-    print v
-    self.lock = True
-    fori = False
-    untilar = []
-    for l in v:
-      #print l
-      if not l.find('For:') == -1 :
-        #pdb.set_trace()
-        fori = True
-        continue
-      if not l.find('Until:') == -1:
-        if len(untilar) > 0:
-          self.insertUntil(untilar)
-          untilar= []
-        ll = l.split(':')
-        untilar.append(ll[1]+':'+ll[2])
+    def setValue(self,v):
+        print v
+        self.lock = True
         fori = False
-        continue
-      if fori:
-        try:
-          index = self.lfordatalist.index(l.lower())
-        except:
-          index = 0
-        self.forcombolist[-1].setCurrentIndex(index)
-        #print self.forcombolist
-        w = QtGui.QComboBox()
-        self.forcombolist.append(w)
-        self.insertFor(w,len(self.forcombolist))
-      else:
-        untilar.append(l)
-    self.insertUntil(untilar)    
-    self.lock = False    
-      
+        untilar = []
+        for l in v:
+            #print l
+            if not l.find('For:') == -1 :
+                #pdb.set_trace()
+                fori = True
+                continue
+
+            if not l.find('Until:') == -1:
+                if len(untilar) > 0:
+                    self.insertUntil(untilar)
+                    untilar= []
+                    ll = l.split(':')
+                    untilar.append(ll[1]+':'+ll[2])
+                    fori = False
+                    continue
+
+            if fori:
+                try:
+                    index = self.lfordatalist.index(l.lower())
+                except:
+                    index = 0
+
+                self.forfieldlist[-1].setValue(self.fordatalist[index])
+                #print self.forcombolist
+                w = FieldChoice('','For:','','',self.fordatalist)
+                self.forfieldlist.append(w)
+                self.insertFor(w)
+
+            else:
+                untilar.append(l)
+
+        self.insertUntil(untilar)
+        self.lock = False
+
       
       
        
@@ -234,50 +234,51 @@ class GForWidget(QtGui.QWidget):
 
 
 if __name__ == "__main__":
-  app = QtGui.QApplication(sys.argv)
-  view = GCompactScheduleWidget('Test')
-  schedarray = ["For: Weekdays Weekends SummerDesignDay",\
-  "Until: 05:00",\
-  "0.05",\
-  "Until: 07:00",\
-  "0.1",\
-  "Until: 08:00",\
-  "0.3",\
-  "Until: 17:00",\
-  "0.9",\
-  "Until: 18:00",\
-  "0.5",\
-  "Until: 20:00",\
-  "0.3",\
-  "Until: 22:00",\
-  "0.2",\
-  "Until: 23:00",\
-  "0.1",\
-  "Until: 24:00",\
-  "0.05",\
-  "For: SummerDesignDay",\
-  "Until: 24:00",\
-  "1.0",\
-  "For: Saturday",\
-  "Until: 06:00",\
-  "0.05",\
-  "Until: 08:00",\
-  "0.1",\
-  "Until: 12:00",\
-  "0.3",\
-  "Until: 17:00",\
-  "0.15",\
-  "Until: 24:00",\
-  "0.05",\
-  "For: WinterDesignDay",\
-  "Until: 24:00",\
-  "0.0",\
-  "For: Sunday Holidays AllOtherDays",\
-  "Until: 24:00",\
-  "0.05"]
-  view.setValue(schedarray)
-  view.setWindowTitle("Widget test")
-  view.show()
-  sys.exit(app.exec_())
-  
-    
+    app = QtGui.QApplication(sys.argv)
+    view = GCompactScheduleWidget('Test')
+    schedarray = ["Through: 12/31",\
+    "For: Weekdays Weekends SummerDesignDay",\
+    "Until: 05:00",\
+    "0.05",\
+    "Until: 07:00",\
+    "0.1",\
+    "Until: 08:00",\
+    "0.3",\
+    "Until: 17:00",\
+    "0.9",\
+    "Until: 18:00",\
+    "0.5",\
+    "Until: 20:00",\
+    "0.3",\
+    "Until: 22:00",\
+    "0.2",\
+    "Until: 23:00",\
+    "0.1",\
+    "Until: 24:00",\
+    "0.05",\
+    "For: SummerDesignDay",\
+    "Until: 24:00",\
+    "1.0",\
+    "For: Saturday",\
+    "Until: 06:00",\
+    "0.05",\
+    "Until: 08:00",\
+    "0.1",\
+    "Until: 12:00",\
+    "0.3",\
+    "Until: 17:00",\
+    "0.15",\
+    "Until: 24:00",\
+    "0.05",\
+    "For: WinterDesignDay",\
+    "Until: 24:00",\
+    "0.0",\
+    "For: Sunday Holidays AllOtherDays",\
+    "Until: 24:00",\
+    "0.05"]
+    view.setValue(schedarray)
+    view.setWindowTitle("Widget test")
+    view.show()
+    sys.exit(app.exec_())
+
+
